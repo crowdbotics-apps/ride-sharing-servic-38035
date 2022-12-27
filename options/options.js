6e22652b802eb9d39a94a3555c4e21c12520756b
Crowdbotics <team@crowdbotics.com>
2022-12-27 22:44:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "ride_sharing_servic_38035",
+  url: "https://ride_sharing_servic_38035.botics.co",
+  api: "https://ride_sharing_servic_38035.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
